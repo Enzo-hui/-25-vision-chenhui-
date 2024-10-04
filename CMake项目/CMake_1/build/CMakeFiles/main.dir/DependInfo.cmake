ff6a9c69c55491d1dc013823bd53a6bd800a2233
陈晖 <14956555+Enzo-hui@user.noreply.gitee.com>
2024-10-04 21:33:14 +0800

    test

---
+++ CMake项目/CMake_1/build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/chen/华南虎视觉/考核项目/CMake项目/CMake_I/main.cpp" "CMakeFiles/main.dir/main.cpp.o" "gcc" "CMakeFiles/main.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/chen/华南虎视觉/考核项目/CMake项目/CMake_I/build/modules/M1/CMakeFiles/M1.dir/DependInfo.cmake"
+  "/home/chen/华南虎视觉/考核项目/CMake项目/CMake_I/build/modules/M2/CMakeFiles/M2.dir/DependInfo.cmake"
+  "/home/chen/华南虎视觉/考核项目/CMake项目/CMake_I/build/common/math/CMakeFiles/Math.dir/DependInfo.cmake"
+  "/home/chen/华南虎视觉/考核项目/CMake项目/CMake_I/build/modules/A1/CMakeFiles/A1.dir/DependInfo.cmake"
+  "/home/chen/华南虎视觉/考核项目/CMake项目/CMake_I/build/modules/A2/CMakeFiles/A2.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
