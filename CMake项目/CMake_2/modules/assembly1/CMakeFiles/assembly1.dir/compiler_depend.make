e05185e246b401c546f24c2f03a696561d22f326
陈晖 <14956555+Enzo-hui@user.noreply.gitee.com>
2024-10-04 21:42:58 +0800

    test

---
+++ CMake项目/CMake_2/modules/assembly1/CMakeFiles/assembly1.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for assembly1.
+# This may be replaced when dependencies are built.
---
+++ CMake项目/CMake_2/modules/assembly1/CMakeFiles/ctest.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ctest.dir/test/assembly1_test.cpp.o"
+  "CMakeFiles/ctest.dir/test/assembly1_test.cpp.o.d"
+  "ctest"
+  "ctest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ctest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
