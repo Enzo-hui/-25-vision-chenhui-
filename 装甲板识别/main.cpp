9588df9c65567d75dbd3fdb05085de6a2784c770
陈晖 <14956555+Enzo-hui@user.noreply.gitee.com>
2024-10-07 11:01:34 +0800

    考核提交

--- 装甲板识别/main.cpp
+++ 装甲板识别/main.cpp
@@ -5,15 +5,14 @@ using namespace cv;
 using namespace std;
 
 condition_variable cond;
-queue<Mat> fpslist; // 线程安全的队列，存储待处理的帧
-atomic<bool> finished = false;//显示任务是否完成
+queue<Mat> fpslist;            // 线程安全的队列，存储待处理的帧
+atomic<bool> finished = false; // 显示任务是否完成
 
-void frameReader(VideoCapture &cap);//帧提取函数的声明
-void frameProcessor();//帧处理函数的声明
+void frameReader(VideoCapture &cap); // 帧提取函数的声明
+void frameProcessor();               // 帧处理函数的声明
 
 int main()
 {
-
 
     // 视频测试
 
@@ -46,21 +45,18 @@ int main()
         th.join();
     }
 
-
-    cap.release();//释放所有空间
+    cap.release(); // 释放所有空间
 
     destroyAllWindows();
     return 0;
 }
-
-
 
 void frameReader(VideoCapture &cap)
 {
     Mat frame;
     while (!finished)
     {
-        
+
         cap >> frame;
         if (frame.empty())
         {
@@ -77,36 +73,35 @@ void frameReader(VideoCapture &cap)
 void frameProcessor()
 {
     Mat frame;
-    
+
     while (!finished || !fpslist.empty())
     {
-        
+
         unique_lock<mutex> lock(mtx);
         cond.wait(lock, []
-                  { return !fpslist.empty() || finished; });
+                  { return !fpslist.empty() || finished; }); // fpslist不为空或finished为false时继续运行否则等待唤醒
 
         if (!fpslist.empty())
         {
-            
 
             // unique_lock<mutex> lock(mtx);
 
             frame = fpslist.front();
             fpslist.pop();
 
-            recognition(frame);//识别灯条
-            
-            matchArmors();//匹配装甲板
-            
-            drawArmor(frame);//画出装甲板
-            
+            recognition(frame); // 识别灯条
+
+            matchArmors(); // 匹配装甲板
+
+            drawArmor(frame); // 画出装甲板
+
             // this_thread::sleep_for(std::chrono::milliseconds(1)); // 等待1毫秒，以便显示帧
             waitKey(20);
             reset();
         }
         else
         {
-            cout<<"视频已结束"<<endl;
+            cout << "视频已结束" << endl;
         }
     }
 }
